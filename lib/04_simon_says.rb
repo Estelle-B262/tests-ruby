4dfec7d0aa8af07fe23c86d1ebd505d38149ff7f
ballifjbe <estelle.ballif@yahoo.com>
2020-03-09 16:39:18 +0100

    final files on gem Rspec training

--- lib/04_simon_says.rb
+++ lib/04_simon_says.rb
@@ -14,3 +14,11 @@ def start_of_word(word, x)
     return word[0,x]
 end
 
+def first_word(word)
+    return word.split.first
+end
+
+def titleize(word)
+    stop_word = %w{a an and the or for of}
+    return word.split.each_with_index.map{|word, index| stop_word.include?(word) && index > 0 ? word : word.capitalize}.join(' ')
+end
